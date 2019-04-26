3ec73c5f72069ac09ec56280dc1f3c0be6da855b
Desmond Mullen <dsm@busylittlestudios.com>
2019-04-26 15:54:10 -0400

    Updates

---
+++ precache-manifest.3be020ddca7742b6fa5e182a38bd1360.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebf07fcacf9e70070b5a51b01a984bb5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aabad2d17a7bd9214802",
+    "url": "/static/css/main.4427edaa.chunk.css"
+  },
+  {
+    "revision": "3ae07c15b85d5d9ccf28",
+    "url": "/static/js/2.627d4df8.chunk.js"
+  },
+  {
+    "revision": "aabad2d17a7bd9214802",
+    "url": "/static/js/main.1ca373fa.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
