5c341565733455099d145b78ab46aacde9575de4
Desmond Mullen <dsm@busylittlestudios.com>
2019-04-26 15:34:03 -0400

    react build

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from '../logo.svg';
 import '../components/components.css';
 
 function Footer() {
     return (
         <footer className='footer'>
-            <img src={logo} className='App-logo' alt='logo' /><em>ClickyPiccy</em> is an exercise in React &bull; &copy; 2019 <a
+            <img src='/assets/images/react-logo-black.png' className='logo' alt='logo' /> <em>ClickyPiccy</em> is an exercise in React &bull; &copy; 2019 <a
                 href='https://desmondmullen.com' target='_blank' rel='noopener noreferrer'>desmondmullen.com</a> &bull; <a href='https://github.com/desmondmullen/clickypiccy' target='_blank' rel='noopener noreferrer'>GitHub Repository</a>
         </footer>
     )
