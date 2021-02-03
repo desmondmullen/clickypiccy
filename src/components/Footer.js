537f456336ddb856ce20f31202dae36a1ba4ff29
Desmond <dsm@desmondmullen.com>
2021-02-03 12:47:45 -0500

    updated footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className='footer'>
             <img src='/assets/images/react-logo-black.png' className='logo' alt='logo' /> <em>ClickyPiccy</em> is an exercise in React &bull; &copy; 2019 <a
-                href='https://desmondmullen.com' target='_blank' rel='noopener noreferrer'>desmondmullen.com</a> &bull; <a href='https://github.com/desmondmullen/clickypiccy/tree/applicationcode' target='_blank' rel='noopener noreferrer'>GitHub Repository</a>
+                href='https://desmondmullen.com' target='_blank' rel='noopener noreferrer'>desmondmullen.com</a> &bull; <a href='https://github.com/desmondmullen/clickypiccy' target='_blank' rel='noopener noreferrer'>GitHub Repository</a>
         </footer>
     )
 };
